f17b528c3e701f6aecf61061dfe64f520f04125c
Laurentiu Badea <laurb9@gmail.com>
2018-07-28 23:37:06 -0700

    Support extracting vobsub, srt and audio from mp4 via MP4Box

--- videosplit.py
+++ videosplit.py
@@ -2,10 +2,12 @@
 """
 Tool to extract all subtitle or audio tracks from a video into separate files.
 
-Requires:
+Requires the following command-line tools to be installed:
 - mkvtoolnix (Mac OS: brew install mkvtoolnix)
-- mediainfo (Mac OS: brew install mediainfo, or install from https://mediaarea.net/en/MediaInfo)
+- mediainfo (Mac OS: brew install mediainfo, or get from App Store via https://mediaarea.net/en/MediaInfo)
+- MP4Box (Mac OS: brew install mp4box)
 """
+from __future__ import print_function
 import argparse
 import itertools
 import json
@@ -23,14 +25,16 @@ except ImportError:
 class VideoExtractor(object):
 
     CODEC_TO_EXT = {
-        "AAC": "m4a",
+        "AAC": "aac",
         "AC3": "ac3",
         "AC-3": "ac3",
         "A_MPEG/L3": "mp3",
-        "S_VOBSUB": "sub",
+        "S_VOBSUB": "idx",
+        "VobSub": "idx",
         "PGS": "sup",
         "S_TEXT/ASS": "sup",
         "S_TEXT": "srt",
+        "tx3g": "srt",
         "AVC": "h264",
         "HEVC": "h265",
     }  # type: Dict[str, str]
@@ -53,7 +57,7 @@ class VideoExtractor(object):
 
     @classmethod
     def format_to_ext(cls, codec, format):
-        # type: (str) -> str
+        # type: (str, str) -> str
         """
         Map a codec name to a file extension to save it to.
 
@@ -69,16 +73,21 @@ class VideoExtractor(object):
 
     @classmethod
     def mediainfo(cls, videoFile):
-        # type: (str) -> Generator[Dict]
+        # type: (unicode) -> (unicode, List[Dict])
         """
         Read the available tracks from video file.
 
         :param videoFile: source video file
         :return: track available as dictionaries {ID, Language, Forced, Extension}
         """
+        media_type = None
+        tracks = []
         mediainfo_cmd = ["mediainfo", "--output=JSON", videoFile]
         mediainfo = json.load(subprocess.Popen(mediainfo_cmd, stdout=subprocess.PIPE, universal_newlines=True).stdout)
         for track in mediainfo["media"]["track"]:
+            if track["@type"] == "General":
+                media_type = track["Format"]
+
             if "ID" in track and track["@type"] != "Menu":
                 #print track
                 track["Extension"] = VideoExtractor.format_to_ext(track["CodecID"], track["Format"])
@@ -92,7 +101,11 @@ class VideoExtractor(object):
                     track["Language"] += "-forced"
                 if "Title" not in track:
                     track["Title"] = ""
-                yield track
+
+                tracks.append(track)
+
+        return media_type, tracks
+
 
     def get_track_name(self, track):
         # type: (dict) -> unicode
@@ -102,38 +115,56 @@ class VideoExtractor(object):
         :return: id-prefixed track name
         """
         if track["Title"]:
+            track["Title"] = track["Title"].replace("/", "-")
             format = u"{ID}.{Title}.{Language}.{Extension}"
         else:
             format = u"{ID}.{Language}.{Extension}"
 
         return format.format(**track)
 
-    def split_mkv(self, videoFile, dst):
+
+    def split_video_file(self, videoFile, dst):
         # type: (str, AnyStr) -> None
         """
         Extract and save tracks of specified type from mkv file.
 
         :param videoFile: source mkv directory
         :param dst: optional directory or filename to save tracks to
-        :param track_types: tracks types to save
-        :param dry_run: if True, no tracks are extracted and the commands are printed instead.
         """
-        basename = os.path.splitext(videoFile)[0]
+        targetName = os.path.splitext(videoFile)[0]
         if dst:
             if os.path.isdir(dst):
-                basename = os.path.join(dst, os.path.basename(basename))
+                targetName = os.path.join(dst, os.path.basename(targetName))
             else:
-                basename = dst
+                targetName = dst
 
-        basename = basename.decode("utf8")
+        targetName = targetName.decode("utf8")
         videoFile = videoFile.decode("utf8")
-        tracks = [u"{id}:{name}.{track}".format(name=basename, track=self.get_track_name(track), **track)
-                  for track in self.mediainfo(videoFile)
-                  if self.should_save_track(track["@type"])]
+        media_type, tracks = self.mediainfo(videoFile)
+        tracks = filter(lambda track: self.should_save_track(track["@type"]), tracks)
+        if media_type == "Matroska":
+            self.split_video_mkv(videoFile, targetName, tracks)
+        elif media_type == "MPEG-4":
+            self.split_video_mp4(videoFile, targetName, tracks)
+        else:
+            print("{0} not supported".format(videoFile, media_type))
+
+
+    def split_video_mkv(self, videoFile, target_name, tracks):
+        # type: (unicode, unicode, List[dict]) -> None
+        """
+        Extract and save tracks of specified type from mkv file.
+
+        :param videoFile: source mkv directory
+        :param dst: optional directory or filename to save tracks to
+        :param tracks: list of selected tracks to extract
+        """
+        tracks = [u"{id}:{name}.{track}".format(name=target_name, track=self.get_track_name(track), **track)
+                  for track in tracks]
 
         cmd = []
         if self.should_save_track("chapters"):
-            cmd += ["chapters", u"{0}.chapters.xml".format(basename)]
+            cmd += ["chapters", u"{0}.chapters.xml".format(target_name)]
 
         if tracks:
             cmd += ["tracks"] + tracks
@@ -142,12 +173,41 @@ class VideoExtractor(object):
             cmd = ["mkvextract", videoFile] + cmd
 
             if self.dry_run:
-                print u" \\\n\t".join(cmd)
+                print(u" \\\n\t".join(cmd))
             else:
                 for line in subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout:
-                    print line,
+                    print(line, end="")
         else:
             print(u"{file}: no {types} tracks found.".format(file=videoFile, types="/".join(self.track_types)))
+
+
+    def split_video_mp4(self, videoFile, target_name, tracks):
+        # type: (unicode, unicode, List[dict]) -> None
+        """
+        Extract and save tracks of specified type from MP4 file.
+
+        :param videoFile: source mkv directory
+        :param dst: optional directory or filename to save tracks to
+        :param tracks: list of selected tracks to extract
+        """
+
+        for track in tracks:
+            track_filename = u"{name}.{track}".format(name=target_name, track=self.get_track_name(track))
+            track_id = str(track["id"]+1)
+            print("Track {0} {1}".format(track_id, track_filename))
+            cmd = ["MP4Box", videoFile]
+            if track["Extension"] == "srt":
+                cmd += ["-std", "-srt", track_id]
+                mp4box = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+                with open(track_filename, "w") as out:
+                    filter(out.write, mp4box.stdout.readlines())
+
+            else:
+                #if track["Extension"] == "sub":
+                #    track_filename = track_filename.replace(".sub", ".idx")
+                cmd = ["MP4Box", videoFile, "-raw", "{0}:output={1}".format(track_id, track_filename)]
+                for line in subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout:
+                    print(line, end="")
 
 
     @staticmethod
@@ -164,7 +224,8 @@ class VideoExtractor(object):
         if os.path.isdir(src):
             for root, dirs, files in os.walk(src):
                 for filename in files:
-                    if filename.endswith(".mkv"):
+                    _, ext = os.path.splitext(filename)
+                    if ext in (".mkv", ".mp4", ".m4v"):
                         yield os.path.join(root, filename)
         else:
             yield src
@@ -191,7 +252,7 @@ def main():
     splitter = VideoExtractor(args.track_types, dry_run=args.dry_run)
     for videoFile in itertools.chain(*(splitter.scan_dir(f) for f in args.files)):
         print("Parsing {0}".format(videoFile))
-        splitter.split_mkv(videoFile, args.dst)
+        splitter.split_video_file(videoFile, args.dst)
 
 
 if __name__ == "__main__":
